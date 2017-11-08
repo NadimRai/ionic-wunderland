1c06cb0836adfa28644ea8c59b8da40ef9212bab
NadimRai <rai_nadim@yahoo.com>
2017-11-09 00:43:40 +0800

    get api key in provider weather.ts

--- src/providers/weather/weather.ts
+++ src/providers/weather/weather.ts
@@ -4,9 +4,16 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class WeatherProvider {
-
+  apiKey = "d2db9c87be811234";
+  url;
   constructor(public http: Http) {
     console.log('Hello WeatherProvider Provider');
+    this.url = 'http://api.wunderground.com/api/'+this.apiKey+'/conditions/q';
+  }
+
+  getWeather(city, state){
+    return this.http.get(this.url+'/'+state+'/'+city+'.json')
+      .map(res => res.json());
   }
 
 }
